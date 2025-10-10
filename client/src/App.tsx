38f607ee4bae493b6e59ce0cdab2b3fbc404b8d2
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-10-10 21:46:56 +0300

    yoooooooooooo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
---
+++ client/src/Home.tsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+  return (
+    <div className='text-6xl text-red-700 font-bold flex flex-cols justify-center items-center h-screen'>Home</div>
+  )
+}
+
+export default Home
