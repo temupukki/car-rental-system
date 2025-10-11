08803d45b6a04654d099d24bd21374c5c3b51d0b
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-10-11 13:31:59 +0300

    update

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,7 +1,8 @@
+import React from 'react'
 
 const Home = () => {
   return (
-    <div className='text-6xl text-red-700 font-bold flex flex-cols justify-center items-center h-screen'>Home</div>
+    <div>Home</div>
   )
 }
 
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return(
+        <>
+        <div>
+            <h1 className="text-red-700 text-4xl">Nav Bar</h1>
+        </div>
+        </>
+    )
+}
