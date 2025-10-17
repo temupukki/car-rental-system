064015f3bab0c2be50d3ef572cacb919218ead80
Temesgen Gashaw <temesgengashaw8@gmail.com>
2025-10-17 17:56:47 +0300

    server-test

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,9 @@
-import express from "express"
+import express, { Request, Response } from "express"
 const app = express();
 const PORT = 3000;
+app.get("/",(req:Request,res:Response)=>{
+    res.json("Abebe tests server now ")
+})
 app.listen(PORT,()=>{
     console.log(`app is runnig On ${PORT}`)
 })
